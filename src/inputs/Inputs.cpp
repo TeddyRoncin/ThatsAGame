036870e6dadb23e723c3d07c0794552be434e66f
Emile Bondu <emile.bondu@gmail.com>
2020-08-30 17:17:46 +0200

    avance encore un peu

---
+++ src/inputs/Inputs.cpp
@@ -0,0 +1,41 @@
+#include "inputs/Inputs.h"
+
+Input::Input()
+     : m_quit(false), m_keys(), m_mouse()
+{
+}
+
+void Input::eventUpdate(SDL_Event events)
+{
+    while( SDL_PollEvent(&events))
+    {
+        switch(events.type)
+        {
+            case SDL_KEYDOWN:
+            case SDL_KEYUP:
+                m_keys.Update(events);
+                break;
+            case SDL_MOUSEMOTION:
+            case SDL_MOUSEWHEEL:
+            case SDL_MOUSEBUTTONDOWN:
+            case SDL_MOUSEBUTTONUP:
+                m_mouse.Update(events);
+                break;
+            case SDL_WINDOWEVENT:
+                m_window.Update(events);
+                break;
+            case SDL_QUIT:
+                m_quit = true;
+                break;
+            default:
+                fprintf(stderr, "Event unandled : " + events.type);
+                break;
+        }
+    }
+}
+
+bool Input::isQuitting()
+{
+    return m_quit;
+}
+
