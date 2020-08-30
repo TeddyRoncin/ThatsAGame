79986c42e5ccb93725e8596986706fbd7b859346
Ota-cueille <vianneyherve3@gmail.com>
2020-08-30 18:05:57 +0200

    modified texture size in main

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[])
 {
     Renderer renderer;
     renderer.Clear();
-    renderer.AddTexture(0,0,"assets/img/test.png");
+    renderer.AddTexture(0,0,"assets/img/test.png",10,10);
     renderer.Render();
     std::cin.get();
     return (0);
